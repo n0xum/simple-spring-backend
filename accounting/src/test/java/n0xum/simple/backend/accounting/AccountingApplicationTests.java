47bdec4bbdce04749258fc3a373bef20b93e4ce3
aleexNxt <alexander.kruska@protonmail.com>
2025-12-02 20:45:53 +0100

    initial commit

---
+++ accounting/src/test/java/n0xum/simple/backend/accounting/AccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package n0xum.simple.backend.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
